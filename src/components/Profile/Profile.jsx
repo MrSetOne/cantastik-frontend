7d27c334d924225e4b8eeb8d3223bffcb47a1ac4
MrSetOne <lara.sanchez.michael@gmail.com>
2022-07-04 14:22:03 +0200

    bugs fixeds

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState} from 'react'
 import {useParams} from 'react-router-dom'
 import { getById } from '../../features/users/usersSlice'
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,20 +9,30 @@ const Profile = () => {
 
     const {id} = useParams()
 
-    const { userDisplayed:target } = useSelector((state)=>state.users)
+    const { userDisplayed:target, isLoading } = useSelector((state)=>state.users)
+    const [load, setLoad] = useState(false)
 
     const dispatch = useDispatch()
 
+    const loadInfo = async() =>{
+      await dispatch(getById(id))
+      setLoad(true)
+    }
+
     useEffect(()=>{
-      dispatch(getById(id))
+      loadInfo()
     },[])
 
-  return (
-    <section className='Profile'>
-        <ProfileCard target={target}/>
 
-    </section>
-  )
+    if(load){
+      return (
+        <section className='Profile'>
+            <ProfileCard target={target}/>
+        </section>
+      )
+    } else {
+      return <h1>cargando...</h1>
+    }
 }
 
 export default Profile
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -2,7 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import userService from './usersService'
 
 const initialState = {
-    userDisplayed: {}
+    userDisplayed: {},
+    isLoading: false
 }
 
 export const getById = createAsyncThunk('users/getById', async(id, thunkAPI) => {
@@ -19,9 +20,14 @@ export const usersSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(getById.fulfilled, (state, action) => {
-                state.userDisplayed = action.payload.foundUser
-            })
+            .addCase(getById.pending, (state) => { state.isLoading = true })
+
+        .addCase(getById.fulfilled, (state, action) => {
+            state.userDisplayed = action.payload.foundUser
+            state.isLoading = false
+        })
+
+        .addCase(getById.rejected, (state, action) => { console.error("Se ha rejectado") })
     },
 })
 
