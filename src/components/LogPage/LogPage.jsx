13770c6f0e79b1a39a6a18d248a9fbee4b64ccb7
MrSetOne <lara.sanchez.michael@gmail.com>
2022-07-12 13:42:50 +0200

    changeSignUp framerMotion

--- src/components/LogPage/LogPage.jsx
+++ src/components/LogPage/LogPage.jsx
@@ -1,25 +1,49 @@
-import { useSelector } from "react-redux"
-import Login from "./Login/Login"
-import SignUp from "./SignUp/SignUp"
-import './LogPage.scss'
-import cantastikGraff from '../../assets/Cantastikgraff.png'
-
-
+import { useSelector } from "react-redux";
+import Login from "./Login/Login";
+import SignUp from "./SignUp/SignUp";
+import "./LogPage.scss";
+import cantastikGraff from "../../assets/Cantastikgraff.png";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const LogPage = () => {
-    
-  const { needSignUp } = useSelector((state) => state.interfaces)
+  const { needSignUp } = useSelector((state) => state.interfaces);
 
   return (
     <section className="LogPage">
       <div className="LogPage__Image">
         <img src={cantastikGraff} alt="Cantastik" />
       </div>
-      <div className="LogPage__Forms">
-        {needSignUp?<SignUp/>:<Login/>}
+      <div className="LogPage__Forms" style={{ position: "relative" }}>
+        <AnimatePresence>
+          {needSignUp && (
+            <motion.div
+              style={{ position: "absolute", width: "70%" }}
+              key={"signUp"}
+              initial={{ x: 0, zIndex: 9 }}
+              animate={{ zIndex: 10, transition: { delay: 0.5 } }}
+              transition={{ duration: 1 }}
+              exit={{ x: [0, -370, 0], zIndex: 10 }}
+            >
+              <SignUp />
+            </motion.div>
+          )}
+          {!needSignUp && (
+            <motion.div
+              style={{ position: "absolute", width: "70%" }}
+              key={"logIn"}
+              initial={{ x: 0, zIndex: 9 }}
+              animate={{ zIndex: 10, transition: { delay: 0.5 } }}
+              transition={{ type: "spring", bounce: 1, duration: 1 }}
+              exit={{ x: [0, -370, 0], zIndex: 10 }}
+            >
+              <Login />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LogPage
+export default LogPage;
--- src/components/LogPage/Login/Login.jsx
+++ src/components/LogPage/Login/Login.jsx
@@ -1,62 +1,94 @@
-import { Button,  Form, Input } from 'antd';
-import {useDispatch} from 'react-redux'
-import { login } from '../../../features/auth/authSlice'
-import {MailOutlined,LockOutlined } from '@ant-design/icons'
+import { Button, Form, Input } from "antd";
+import { useDispatch } from "react-redux";
+import { login } from "../../../features/auth/authSlice";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 import { changeNeedSignUp } from "../../../features/interface/interfacesSlice";
-import './Login.scss'
-
+import "./Login.scss";
 
 const Login = () => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch();  
+  const onFinish = (values) => {
+    dispatch(login(values));
+  };
 
-    const onFinish = (values) => {
-        dispatch(login(values))
-      };
-    
-      const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-      };
-  
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div className='Login' >
-      <h2>Iniciar sesión</h2>
-            <Form
-      name="login"
-      className="login-form"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
+    <div
+      className="Login"
+      style={{
+        height: 585,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
     >
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-        style={{display:'flex',justifyContent:'center'}}
+      <h2>Iniciar sesión</h2>
+      <Form
+        name="login"
+        className="login-form"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input prefix={<MailOutlined className="site-form-item-icon" style={{color:"gray"}} />} placeholder="Tu email"/>
-      </Form.Item>
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your username!" }]}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Input
+            prefix={
+              <MailOutlined
+                className="site-form-item-icon"
+                style={{ color: "gray" }}
+              />
+            }
+            placeholder="Tu email"
+          />
+        </Form.Item>
 
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-        style={{display:'flex',justifyContent:'center'}}
-      >
-        <Input.Password prefix={<LockOutlined  className="site-form-item-icon" style={{color:"gray"}}/>} placeholder="Tu contraseña"/>
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Input.Password
+            prefix={
+              <LockOutlined
+                className="site-form-item-icon"
+                style={{ color: "gray" }}
+              />
+            }
+            placeholder="Tu contraseña"
+          />
+        </Form.Item>
 
-      <Form.Item >
-      <Button style={{width:"100%"}} type="primary" htmlType="submit" className="login-form-button">
-          Iniciar sesión
-        </Button>
-      </Form.Item>
-      <Form.Item className='login-form--lastItem' >
-      <Button style={{width:"100%", marginBottom:0}} type="default" className="login-form-button" onClick={()=>dispatch(changeNeedSignUp())}>
-          Crear cuenta
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            style={{ width: "100%" }}
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Iniciar sesión
+          </Button>
+        </Form.Item>
+        <Form.Item className="login-form--lastItem">
+          <Button
+            style={{ width: "100%", marginBottom: 0 }}
+            type="default"
+            className="login-form-button"
+            onClick={() => dispatch(changeNeedSignUp())}
+          >
+            Crear cuenta
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
